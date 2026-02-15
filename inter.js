1e200c8dff8179cc5a2568d756f961569f4a39b1
Theerth1 <theerth.srinivasan@gmail.com>
2026-02-15 16:49:15 -0500

    creation and additions to html, css, js, and png

---
+++ inter.js
@@ -0,0 +1,17 @@
+const leftClick = document.getElementById("leftClick");
+const rightClick = document.getElementById("rightClick");
+const infoButton = document.getElementById("info");
+const movesButton = document.getElementById("moves");
+const title = document.getElementById("title");
+const details = document.getElementById("details");
+
+let currId = 1;
+
+async function retrieveData(id) {
+    const picture;
+    const typePicture;
+    let infoList = [];
+    let movesList = [];
+    const name;
+
+}
