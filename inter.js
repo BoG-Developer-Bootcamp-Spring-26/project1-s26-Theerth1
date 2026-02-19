7e3d6ed6bb886b3d366b0dec53a0eb1d61215db4
Theerth1 <theerth.srinivasan@gmail.com>
2026-02-19 00:35:48 -0500

    deleted past hardcoded positions and moved to flex boxes. need to fix right side still

--- inter.js
+++ inter.js
@@ -5,13 +5,31 @@ const movesButton = document.getElementById("moves");
 const title = document.getElementById("title");
 const details = document.getElementById("details");
 
+let picture;
+let typePicture;
+let height;
+let weight;
+let hp;
+let attack;
+let defense;
+let sa;
+let sd;
+let speed;
+let moves = [];
+
+const colorMatch = {
+    Normal: A8A77A,
+    Fire: EE8130,
+    Water: 6390F0,
+    Electric: F7D02C,
+    Grass: 7AC74C,
+    Ice: 96D9D6,
+    
+}
+
+
 let currId = 1;
 
 async function retrieveData(id) {
-    const picture;
-    const typePicture;
-    let infoList = [];
-    let movesList = [];
-    const name;
 
 }
